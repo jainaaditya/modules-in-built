99da37aad1c9e5f1abf575990a50b3d87e3e4501
jainaaditya <42334537+jainaaditya@users.noreply.github.com>
2018-09-01 10:49:00 +0530

    Add files via upload

---
+++ commanmodule.py
@@ -0,0 +1,19 @@
+#ques 1
+import datetime as dt
+x = dt.datetime.now()
+print("today day is",x.strftime("%A"))
+
+#ques2
+import webbrowser
+youtube=input("enter your search:")
+webbrowser.open_new_tab('https://www.youtube.com/=%s'%youtube)
+
+#ques3
+import os
+path="D:\python\assignment\comman module"
+filename=listdir('.')
+i=1
+for file in filename:
+        a=input("name of file")
+        os.rename(os.path.join(path,file),os.path.join(path,a+'.jpg'))
+        i=i+1
